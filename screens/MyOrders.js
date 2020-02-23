8f0be8c3b2f5ae7eda4e6d015eddb8b317f8cf4a
Andrew Walker <Andrew_Walker2@baylor.edu>
2020-02-22 19:23:11 -0600

    Idk what these changes are

--- screens/MyOrders.js
+++ screens/MyOrders.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {
-  ScrollView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import {ListItem, Icon} from 'react-native-elements';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import axios from 'axios';
 import {AsyncStorage, Platform} from 'react-native';
 
@@ -60,14 +61,15 @@ class MyOrders extends React.Component {
           <View style={styles.container}>
             {
               list.map((item, i) => (
-                
-                <ListItem
+                <OptionButton
                   key={i}
-                  title={item.title}
-                  subtitle={item.description}
-                  height= {60}
-                  onPress={() => null}
-                  keyExtractor={item => item.id}
+                  icon="md-pizza"
+                  title={item.title + " - " + item.status}
+                  label={item.description}
+                  onPress={() => {
+                      null
+                    }
+                  }
                 />
               ))
             }
@@ -78,92 +80,113 @@ class MyOrders extends React.Component {
   }
 }
 
+function OptionButton({ icon, label, onPress, isLastOption, title, id }) {
+  return (
+    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.optionIconContainer}>
+          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
+        </View>
+        <View style={styles.optionTextContainer}>
+          <LabelForInput customLabel={title} />
+          <LabelForInput customLabel={label} />
+        </View>
+      </View>
+    </RectButton>
+  );
+}
+
+function LabelForInput({customLabel}){
+  return (
+    <Text style={styles.optionSmallHeadingText}>
+      {customLabel}
+    </Text>)
+    ;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+    backgroundColor: '#fafafa',
   },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 15,
   },
   welcomeContainer: {
     alignItems: 'center',
     marginTop: 10,
     marginBottom: 20,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  optionIconContainer: {
+    marginRight: 12,
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
     paddingVertical: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: 0,
+    borderColor: '#ededed',
   },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  lastOption: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  optionText: {
+    fontSize: 15,
+    alignSelf: 'flex-start',
+    marginTop: 1,
+  },
+  optionButton: {
+    fontSize: 12,
+    marginTop: 1,
+  },
+  optionSubheadingText: {
+    textAlign: 'center',
+    fontSize: 24,
+    marginTop: 15,
+    marginBottom: 15,
+    textDecorationLine: 'underline',
+  },
+  optionMultipleButtons: {
+    flexDirection: "row",  
+    justifyContent: 'space-evenly',
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  optionMultipleInputs: {
+    flexDirection: "row",  
+    justifyContent: 'space-evenly',
+  },
+  containerInput: {
+    paddingTop: 8,
+    paddingBottom: 1,
+    paddingHorizontal: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  containerMultipleInput: {
+    paddingTop: 8,
+    paddingBottom: 8,
+    maxWidth: 180,
+    justifyContent: "center",
+  },
+  optionSmallHeadingText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+    paddingLeft: 10,
+    fontStyle: 'italic'
+  },
+  messageText: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  containerStacked: {
+    flex: 1,
+    paddingTop: 20,
   },
 });
 
